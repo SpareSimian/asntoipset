8ea1ada74869c76abcc4a27d38884090f7430e91
Kenneth Porter <ken@rocinante.lsi.lan>
2022-11-30 20:17:07 -0800

    Update docstring from README.md.

--- asntoipset.py
+++ asntoipset.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 """ Convert a list of records from iptoasn.com to a firewalld ipset XML file.
 
-Download https://iptoasn.com/data/ip2asn-v4.tsv.gz, uncompress, filter
-with grep, and feed to this script to generate a file to place in
-/etc/firewalld/ipsets for use in firewall rules.
+Download a database in tab-delimited format from https://iptoasn.com/,
+uncompress, filter with grep, and feed to this script to generate a
+file to place in /etc/firewalld/ipsets for use in firewall rules.
 
 Example:
 
 wget -q -O - https://iptoasn.com/data/ip2asn-v4.tsv.gz | \
-    gunzip | grep "DIGITALOCEAN" | ./asntoipset.py > /etc/firewalld/ipsets/DigitalOcean.xml
+    gunzip | grep -v "DIGITALOCEAN" | ./asntoipset.py > /etc/firewalld/ipsets/DigitalOcean.xml
 
-To match country codes, try 'grep -v -P "\tUS\t"' as your filter.
+To match country codes to block all but addresses in the US, try the
+following as your filter. (Include None to include non-routable blocks
+such as your LAN or you might lock yourself out of your server.)
+
+grep -v -P "\t(US|None)\t"
 
 Requires the netaddr package to extract CIDR lists from a range of IP addresses.
 Requires ElementTree from Python > 3.7 that includes the indent method.
